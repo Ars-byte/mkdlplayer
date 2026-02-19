f6f99b1c6fc1787cc6897c325ab9110412e3a469
Ars-btw <shaggyfino11@gmail.com>
2026-02-19 20:29:16 -0300

    Add script to set up Python virtual environment

---
+++ steamdeck_fix.sh
@@ -0,0 +1,5 @@
+python -m venv --without-pip venv
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+./venv/bin/python get-pip.py
+./venv/bin/pip install pygame PyQt6 pypresence mutagen soundfile numpy
+./venv/bin/python mkdlplayer.py
