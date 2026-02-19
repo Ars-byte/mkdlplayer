dea486b9b5344b6bb6b67123aba850443d410ef4
Ars-btw <shaggyfino11@gmail.com>
2026-02-19 20:42:24 -0300

    Update steamdeck_fix.sh

--- steamdeck_fix.sh
+++ steamdeck_fix.sh
@@ -2,4 +2,3 @@ python -m venv --without-pip venv
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 ./venv/bin/python get-pip.py
 ./venv/bin/pip install pygame PyQt6 pypresence mutagen soundfile numpy
-./venv/bin/python mkdlplayer.py
